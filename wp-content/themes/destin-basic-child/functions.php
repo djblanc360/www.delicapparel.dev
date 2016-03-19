2158a0e1a55ca1e5a930102b66dc3915e76e3324
Hans Xavier <xavier.hans@epitech.eu>
2016-03-18 19:04:58 -0800

    Use category instead of post on the frontpage

--- wp-content/themes/destin-basic-child/functions.php
+++ wp-content/themes/destin-basic-child/functions.php
@@ -14,3 +14,46 @@ endif;
 add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css' );
 
 // END ENQUEUE PARENT ACTION
+
+function delicaparel_front_page_render() {
+	global $wp_query, $paged;
+	$args = array(
+		'posts_per_page' => 100,
+		'post_type' => 'category'
+	);
+	query_posts($args);
+	?>
+	<?php while ( have_posts() ) : the_post(); ?>
+		<?php if ( is_front_page() ) { ?>
+		<div class="item">
+			<figure class="effect-julia">
+				<?php
+				if ( has_post_thumbnail() )
+					the_post_thumbnail( 'home' );
+				else
+					echo '<img src="' . BAVOTASAN_THEME_URL . '/library/images/no-image.jpg" alt="" />';
+				?>
+				<figcaption>
+					<h2><?php the_title(); ?></h2>
+					<div>
+						<p><?php echo wp_trim_words( strip_shortcodes( get_the_excerpt() ) , 10 ); ?></p>
+						<p class="more-link-p"><?php _e( 'Continue reading <span class="meta-nav">&rarr;</span>', 'destin' ); ?></p>
+					</div>
+					<a href="<?php the_permalink(); ?>"><?php _e( 'View more', 'destin' ); ?></a>
+				</figcaption>
+			</figure>
+		</div>
+		<?php } else { ?>
+			<?php get_template_part( 'content', get_post_format() ); ?>
+		<?php } ?>
+	<?php endwhile; ?>
+	<?php
+}
+
+add_theme_support( 'infinite-scroll', array(
+	    'type' => 'scroll',
+		'wrapper' => false,
+		'footer' => false,
+		'footer_widgets' => 'extended-footer',
+        'render'=> 'delicaparel_front_page_render',
+	) );
