03e8cb2d138ff3b3758d2d9143d6363a125afb9b
Hans Xavier <xavier.hans@epitech.eu>
2016-04-08 11:47:41 -0700

    Category page

--- wp-content/themes/destin-basic-child/single-category.php
+++ wp-content/themes/destin-basic-child/single-category.php
@@ -12,6 +12,37 @@
  * @since 1.0.0
  */
 get_header(); ?>
-<div class="container">
+<div id="category-page" class="container category-apparels-container">
+	<?php
+	$section = $wp_query->post->post_title;
+	$args = array(
+		'posts_per_page' => 15,
+		'post_type' => 'apparel',
+		'meta_query'	=> array(
+			array(
+				'key'		=> 'sections',
+				'compare'	=> 'LIKE',
+				'value'		=> ';' . $section . ';',
+			)
+		)
+	);
+	query_posts($args);
+	while ( have_posts() ) : the_post(); ?>
+		<div class="apparel">
+	        <div data-product-image-hover="0">
+	        	<?php echo '<a href="' . get_permalink($post->ID) . '" class="product-title">';?>
+	  				<?php
+						$picture = get_field('image');
+						if (!empty($picture))
+							echo '<img src="' . $picture['url'] . '" alt="' . $picture['alt'] . '" />';
+						else
+							echo '<img src="' . BAVOTASAN_THEME_URL . '/library/images/no-image.jpg" alt="" />';
+					?>
+	            </a>
+	        </div>
+	        <?php echo '<a href="' . get_permalink($post->ID) . '" class="product-title">' . the_title() . '</a>';?>
+        	<div class="product-price">$44.99</div>
+		</div>
+	<?php endwhile; ?>
 </div>
 <?php get_footer(); ?>
